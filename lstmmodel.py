64e8f33cdde31ca7475ef50108dd19b11ae7c33d
Shaunak Inamdar <65210567+ShaunakInamdar@users.noreply.github.com>
2020-07-27 19:51:42 +0530

    Create lstmmodel.py

---
+++ lstmmodel.py
@@ -0,0 +1,37 @@
+# model LSTM
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Masking, Embedding
+
+model = Sequential()
+
+# Embedding layer
+model.add(
+    Embedding(input_dim=num_words,
+              input_length = training_length,
+              output_dim=100,
+              weights=[embedding_matrix],
+              trainable=False,
+              mask_zero=True))
+
+# Masking layer for pre-trained embeddings
+'''
+I was asking about this masking layer, i dont know what it does and if we need it.
+'''
+model.add(Masking(mask_value=0.0))
+
+# Recurrent layer
+model.add(LSTM(64, return_sequences=False, 
+               dropout=0.1, recurrent_dropout=0.1))
+
+# Fully connected layer
+model.add(Dense(64, activation='relu'))
+
+# Dropout for regularization
+model.add(Dropout(0.5))
+
+# Output layer
+model.add(Dense(num_words, activation='softmax'))
+
+# Compile the model
+model.compile(
+    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
